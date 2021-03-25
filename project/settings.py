f4c21008900573543578e545a3f276a7815da01a
Lucas Santus <leos9877@gmail.com>
2021-03-25 11:58:56 -0300

    adicionando alterações

--- project/settings.py
+++ project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'materializecssform',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,7 +43,7 @@ INSTALLED_APPS = [
 INSTALLED_APPS += [
     'administracao',
     'cadastro',
-    'widget_tweaks',
+
 ]
 
 MIDDLEWARE = [
