65865feea3e9ad850969030d094d981d4e978c3a
Lucas Santos <leos9877@gmail.com>
2021-03-26 15:21:55 -0300

    organizando projeto

---
+++ cadastro/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from cadastro.views import *
+
+urlpatterns = [
+    path("registrar-pessoa/", registrar_pessoa, name="registrar_pessoa"),
+    path("registrar-votacao/", registrar_votacao, name="registrar_votacao"),
+    path("registrar-opcao-voto/", registrar_opcao, name="registrar_opcao"),
+    path("listar-pessoas/", listar_pessoas, name="listar_pessoas"),
+]
--- project/urls.py
+++ project/urls.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from cadastro.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", index, name="index"),
-    path("registrar-pessoa/", registrar_pessoa, name="registrar_pessoa"),
-    path("registrar-votacao/", registrar_votacao, name="registrar_votacao"),
-    path("registrar-opcao-voto/", registrar_opcao, name="registrar_opcao"),
-    path("listar-pessoas/", listar_pessoas, name="listar_pessoas"),
+    path('', include('home.urls')),
+    path('cadastro/', include('cadastro.urls')),
+    path('administracao/', include('administracao.urls')),
 ]
