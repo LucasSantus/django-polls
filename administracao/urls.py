2b19ed3ecca02a382d7ea509acaeca8ab2efc1bd
Lucas Santos <leos9877@gmail.com>
2021-03-30 16:55:41 -0300

    função apuração funcionando

--- administracao/urls.py
+++ administracao/urls.py
@@ -4,6 +4,7 @@ from .views import *
 
 urlpatterns = [
     path("votar/<int:id_votacao>/<int:id_pessoa>", votar, name="votar"),
-    path("apuracao/<int:id_votacao>", apuracao, name="apuracao"),
     path("validacao/<int:id_votacao>", validacao, name="validacao"),
+    path("apuracao/<int:id_votacao>", apuracao, name="apuracao"),
+    path("detalhe-apuracao/<int:id_votacao>", detalhe_apuracao, name="detalhe_apuracao"),
 ]
--- administracao/views.py
+++ administracao/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import *
 from django.contrib import messages
-from .models import Pessoa_Voto
+from cadastro.models import *
 
 def validacao(request, id_votacao):
     if request.POST:
@@ -10,16 +10,10 @@ def validacao(request, id_votacao):
             votacao = Votacao.objects.get(pk=id_votacao)
             pessoa = Pessoa.objects.get(cpf=cpf)
             
-            if votacao.voto_unico:
-                pessoa_voto = Pessoa_Voto.objects.get(votacao=votacao, pessoa=pessoa)
-
-                messages.error(request, "Você ja votou seu safado")
+            return redirect("votar", votacao.id, pessoa.id)
             
         except Pessoa.DoesNotExist: 
             messages.error(request, "CPF não cadastrado!")
-        
-        except Pessoa_Voto.DoesNotExist: 
-           return redirect("votar", id_votacao, pessoa.id)
          
     return render(request, "administracao/validacao.html")
 
@@ -29,6 +23,10 @@ def votar(request, id_votacao, id_pessoa):
     votacao = Votacao.objects.get(pk=id_votacao)
     listOpcaoVoto = OpcaoVoto.objects.filter(votacao=votacao)
 
+    if Pessoa_Voto.objects.filter(pessoa=pessoa,votacao=votacao) and votacao.voto_unico == True:
+        messages.error(request,"O voto é unico!")
+        return redirect("index") 
+
     if request.POST:
         idOpcaoVoto = request.POST.get('voto', None)
         objOpcaoVoto = OpcaoVoto.objects.get(pk=idOpcaoVoto)
@@ -36,15 +34,16 @@ def votar(request, id_votacao, id_pessoa):
         try:
             voto = Pessoa_Voto.objects.get(votacao=votacao, pessoa=pessoa, opcao=objOpcaoVoto)
             
-            objOpcaoVoto.quantidade_votos += 1
+            objOpcaoVoto.numero_votos += 1
             objOpcaoVoto.save()
             
         except Pessoa_Voto.DoesNotExist:
             voto = Pessoa_Voto()
-            voto.pessoa = pessoa
-            voto.opcao = objOpcaoVoto
-            voto.votacao = votacao
-            voto.quantidade_votos +=1
+            
+        voto.pessoa = pessoa
+        voto.opcao = objOpcaoVoto
+        voto.votacao = votacao
+        voto.quantidade_votos +=1
             
 
         voto.save()
@@ -72,12 +71,12 @@ def apuracao(request, id_votacao):
 
 def detalhe_apuracao(request, id_votacao):
         
-    votacao = Votacao.objects.get(pk=id_votacao)
+    opcao = OpcaoVoto.objects.get(pk=id_votacao)
 
-    votos = OpcaoVoto.objects.filter(votacao=votacao)
+    opcoes = Pessoa_Voto.objects.filter(opcao=opcao)
 
     context = {
-        "votos": votos,
+        "opcoes": opcoes,
     }
 
-    return render(request, "administracao/apuracao.html", context)
+    return render(request, "administracao/detalhe_apuracao.html", context)
