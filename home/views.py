00dbdae5ab8ed78e47bfe1e1f1c9a0738e22659d
Lucas Santos <leos9877@gmail.com>
2021-06-19 11:43:29 -0300

    inserindo alterações ao projeto

--- home/views.py
+++ home/views.py
@@ -1,13 +1,18 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from cadastro.models import Votacao
 from django.utils import timezone
+from datetime import date
+
+def base(request):
+    context = { 
+    	'date': date.today()
+    }
+    return context
+
 
 def index(request):
-
     votacoes = Votacao.objects.filter(data_inicio__lte=timezone.now(), data_fim__gte=timezone.now())
-    
     context = {
         "votacoes": votacoes,
     }
-
     return render(request, "home/index.html", context)
