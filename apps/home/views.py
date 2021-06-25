a84035dcdf8b1714e3866798a51b7b0727c12fad
Lucas Santos <leos9877@gmail.com>
2021-06-25 07:39:34 -0300

    inserindo alterações as cores

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,21 +1,19 @@
 from django.shortcuts import render
-from cadastro.models import Votacao
 from django.utils import timezone
 from datetime import date
+from django.http import JsonResponse
+from cadastro.models import Votacao
+from .colors import Color
 
 def base(request):
-
-    colors = {
-        'primary_class': "red",
-        'primary_style': "deep-purple accent-2",
-    }
-
+    get_colors = Color.get_colors()
+    colors = get_colors["main"]
+        
     context = { 
         'date': date.today(),
         'colors': colors,
     }
     return context
-
 
 def index(request):
     votacoes = Votacao.objects.filter(data_inicio__lte=timezone.now(), data_fim__gte=timezone.now())
@@ -23,3 +21,26 @@ def index(request):
         "votacoes": votacoes,
     }
     return render(request, "home/index.html", context)
+
+def mode(request):
+    
+    try:
+        mode = request.GET.get('switch-check', False)
+    except:
+        mode = {}
+        
+    if mode == "on":
+        print("\n\n\ndark\n\n\n")
+    else: 
+        print("\n\n\nmain\n\n\n")
+        
+    data = {
+        'mode': mode,
+    }
+
+    print("\n\n\n")
+    print(mode)
+    print("\n\n\n")
+    
+    return JsonResponse(data)
+    
