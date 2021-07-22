d410082a71260a6714b2a63d534afe9eceb91690
Lucas Santus <leos9877@gmail.com>
2021-07-22 18:24:45 -0300

    :bug: - corrigindo bug da tabela.

--- static/js/index.js
+++ static/js/index.js
@@ -1,35 +1,3 @@
-
-$(document).ready(function(){
-    /* Inicialização do modal */
-    $('.modal').modal();
-
-    /* Inicialização do tooltipped */
-    $('.tooltipped').tooltip({delay: 50});
-
-    /* Inicialização do chip */
-    $('.chips').material_chip();
-
-    /* Inicialização do sidenav */
-    $('.sidenav').sidenav();
-
-    /* Inicialização do select */
-    $('select').formSelect();
-
-    /* Inicialização do dropdown */
-    $('.dropdown-trigger').dropdown({
-        coverTrigger: false,
-        hover: true,
-    });
-
-    // Menu flutuante 
-    $('.fixed-action-btn').floatingActionButton({
-        direction: 'left'
-    }); 
-    
-    //Collapsible
-    $('.collapsible').collapsible();
-});
-
 function validacao_data(){
     var data_inicial = new Date($("input[name='data_inicio']").val());
     var data_final = new Date($("input[name='data_fim']").val());
