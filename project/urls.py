f5dd34332b72af8f3a1ee6b8c8a4cd03faed3b98
Lucas Santos <leos9877@gmail.com>
2022-08-26 00:02:47 -0300

    :pushpin: change codes in project/urls.py

--- project/urls.py
+++ project/urls.py
@@ -1,27 +1,33 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
-from . import settings_local
+from django.urls import path, include
+from django.urls.conf import re_path
 from django.conf import settings
 from django.views.static import serve
 from django.conf.urls.static import static
+from project.settings import DEBUG
 
 urlpatterns = [
-    # Admin
+    # ADMIN
     path('admin/', admin.site.urls),
-
-    # Include URL APP's
+    
+    # INCLUDE APPS
     path('', include('home.urls')),
-    path('usuario/', include('usuarios.urls')),
-    path('', include('votacao.urls')),
+    # path('', include('votations.urls')), 
+    path('', include('users.urls')), 
+]
 
-    #Config URL's
+# INCLUDE STATIC
+urlpatterns += [
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# INCLUDE MEDIA
+urlpatterns += [
     re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
-if settings_local.DEBUG:
-    import debug_toolbar
-
+if DEBUG:
+    import debug_toolbar 
     urlpatterns += [
         path('__debug__/', include(debug_toolbar.urls)),
-    ] 
+    ]
