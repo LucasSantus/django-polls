f51c6e190fb1211edb584fe45b1706541d4c2d6a
Lucas Santos <leos9877@gmail.com>
2022-08-30 01:16:15 -0300

    :pushpin: implement code in urls apps/votations/urls/rooms.py

--- apps/votations/urls/rooms.py
+++ apps/votations/urls/rooms.py
@@ -4,7 +4,7 @@ from votations.views import create_room, change_room, desactivate_room
 urlpatterns = [
     path('rooms/', include([
         path("create/", create_room, name="create_room"),
-        path("change/<slug:slug_room>/", change_room, name="change_room"),
+        path("change/<int:id_room>/", change_room, name="change_room"),
         path("desactivate/<int:id_room>/", desactivate_room, name="desactivate_room"),
         # path("connect/", connect_room, name="connect_room"),
     ])),
