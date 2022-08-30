2275f84370e3b638b45ca368f1b5ef30f01d3fb2
Lucas Santos <leos9877@gmail.com>
2022-08-30 01:16:16 -0300

    :rocket: add new files apps/home/validate/

---
+++ apps/home/validate/extras.py
@@ -0,0 +1,6 @@
+
+# def user_permissions(user_permission_id, user_id):
+#     def in_groups(u):
+#         if bool(u.groups.filter(name__in=group_names)): return True
+#         return False
+#     return user_passes_test(in_groups, login_url="ViewHome")
