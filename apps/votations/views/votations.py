b8520f6668cc559a13324c4ee686942896014eb0
Lucas Santos <leos9877@gmail.com>
2022-09-04 02:48:59 -0300

    :rocket: insert new file apps/votations/views/votations.py

---
+++ apps/votations/views/votations.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
+from votations.models import Room
+from links.models import UserRooms
+
+@login_required
+def votations_room(request, slug_room):
+    room = Room.get_room(request, slug_room)
+
+    votations = Votation.objects.filter(room_slug = room.slug, is_active = True)
+
+    context = {
+        "votations": votations,
+    }
+
+    return render(request, "votations/votations/votations_room.html", context)
+
+@login_required
+def create_votation(request, slug_room):
+    room = Room.get_room(request, slug_room)
+
+    form = VotationForm()
+
+    if request.POST:
+        form = VotationForm(request.POST)
+        if form.is_valid():
+            votation = form.save(commit = False)
+            votation.room = room
+            votation.save()
+
+            messages.success(request, DEFAULT_MESSAGES['ADD'])
+            return redirect("votations_room", slug_room)
+
+    context = {
+        "form": form,
+    }
+
+    return render(request, "votations/votations/create_votation.html", context)
