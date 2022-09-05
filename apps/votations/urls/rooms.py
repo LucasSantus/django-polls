6b2903034c3e2d46cd05262bba45beca0ac3c9ec
Lucas Santos <leos9877@gmail.com>
2022-09-05 00:04:44 -0300

    :pushpin: change file apps/votations/urls/rooms.py

--- apps/votations/urls/rooms.py
+++ apps/votations/urls/rooms.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
-from votations.views import create_room, change_room, desactivate_room, connect_room
+# from votations.views import create_room, change_room, desactivate_room, connect_room
+from votations.views import *
 
 urlpatterns = [
     path('rooms/', include([
@@ -7,5 +8,12 @@ urlpatterns = [
         path("change/<slug:slug_room>/", change_room, name="change_room"),
         path("desactivate/<slug:slug_room>/", desactivate_room, name="desactivate_room"),
         path("connect/", connect_room, name="connect_room"),
+    ])),
+
+    path('votations/', include([
+        path("room/<slug:slug_room>/", votations_room, name="votations_room"),
+        path("create/", create_votation, name="create_votation"),
+        # path("change/<slug:slug_votation>/", change_votation, name="change_votation"),
+        # path("desactivate/<slug:slug_votation>/", desactivate_votation, name="desactivate_votation"),
     ])),
 ]
